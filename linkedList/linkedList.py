547538e1de76658fc2318dd810df8e7a1e75bf5c
Jesus <jesus.tdd03@gmail.com>
2025-04-24 21:38:04 +0100

    Implemented method add.

--- linkedList/linkedList.py
+++ linkedList/linkedList.py
@@ -20,6 +20,24 @@ class LinkedList:
             current = current.next
 
 
+    def add(self, *items):
+        if len(items) > 1:
+            for item in items:
+                if not self.add(item):
+                    return False
+            return True
+
+        new_item = Item(*items)
+        if self.isEmpty():
+            self.__first = new_item
+        else:
+            new_item.prev = self.__last
+            self.__last.next = new_item
+        self.__last = new_item
+        self.__size += 1
+        return True
+
+
     def isEmpty(self):
         return len(self) == 0
 
