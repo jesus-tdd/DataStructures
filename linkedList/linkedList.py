ba664f42e71fed871da21de921db0b062aeb6768
Jesus <jesus.tdd03@gmail.com>
2025-04-24 20:53:20 +0100

    Implemented magic methods and stuff.

---
+++ linkedList/linkedList.py
@@ -0,0 +1,31 @@
+from linkedListItem import LinkedListItem as Item
+
+class LinkedList:
+    def __init__(self, *items):
+        self.__first = None
+        self.__last = None
+        self.__size = 0
+        if len(items) > 0:
+            self.add(items)
+
+
+    def isEmpty(self):
+        return len(self) == 0
+
+
+    def __len__(self):
+        return self.__size
+
+
+    def __str__(self):
+        if self.isEmpty():
+            return "[]"
+
+        string = "["
+        """for item in self.items():
+            string += str(item)+ ", " """
+        string = string[:-2] + "]"
+        return string
+
+if __name__ == "__main__":
+    print(LinkedList(1,2,3))
